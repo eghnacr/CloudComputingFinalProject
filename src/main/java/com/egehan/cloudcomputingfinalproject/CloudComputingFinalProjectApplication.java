24da0e0f136dbdf6abecf65f401a059447b360da
eghnacr <eghnacr@gmail.com>
2022-01-29 16:02:23 +0300

    initial commit

---
+++ src/main/java/com/egehan/cloudcomputingfinalproject/CloudComputingFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.egehan.cloudcomputingfinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudComputingFinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudComputingFinalProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/egehan/cloudcomputingfinalproject/CloudComputingFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egehan.cloudcomputingfinalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudComputingFinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
